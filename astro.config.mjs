03367014d788dc8f757fa833f33bc419cf2a643b
Mohan Gupta <mohangupta@live.com>
2026-04-29 16:34:56 +1000

    keyboard shortcuts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,36 +2,46 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mohankumargupta.github.io',
-	base: '/tinkercad',
-	integrations: [
-		starlight({
-			title: 'Tinkercad Tutorial',
-		
-			components: {
-				ThemeProvider: './src/components/ThemeProvider.astro'
-			},
-			customCss: ['/src/styles/custom.css'],
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/mohankumargupta/tinkercad' }],
-			sidebar: [
-				// {
-				// 	label: 'Guides',
-				// 	items: [
-				// 		// Each item here is one entry in the navigation menu.
-				// 		{ label: 'Example Guide', slug: 'guides/example' },
-				// 	],
-				// },
-				{
-					label: 'Examples',
-					autogenerate: { directory: 'examples' },
-				},
-				{
-					label: 'Fundamentals',
-					autogenerate: { directory: 'fundamentals' },
-				},				
-			],
-		}),
+  site: 'https://mohankumargupta.github.io',
+  base: '/tinkercad',
+
+  integrations: [
+      starlight({
+          title: 'Tinkercad Tutorial',
+      
+          components: {
+              ThemeProvider: './src/components/ThemeProvider.astro'
+          },
+          customCss: [
+            '/src/styles/custom.css',
+            './src/styles/global.css',
+        ],
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/mohankumargupta/tinkercad' }],
+          sidebar: [
+              // {
+              // 	label: 'Guides',
+              // 	items: [
+              // 		// Each item here is one entry in the navigation menu.
+              // 		{ label: 'Example Guide', slug: 'guides/example' },
+              // 	],
+              // },
+              {
+                  label: 'Examples',
+                  autogenerate: { directory: 'examples' },
+              },
+              {
+                  label: 'Fundamentals',
+                  autogenerate: { directory: 'fundamentals' },
+              },				
+          ],
+      }),
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
