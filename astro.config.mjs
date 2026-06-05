9a6e5e46689ba32c90f568992ae1e1887243039e
Mohan Gupta <mohangupta@live.com>
2026-06-05 13:11:54 +1000

    changes from feedback

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
 
   integrations: [
       starlight({
-          title: 'Tinkercad Tutorial',
+          title: 'Tinkercad With Worked Examples',
       
           components: {
               ThemeProvider: './src/components/ThemeProvider.astro'
